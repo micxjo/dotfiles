9feac85235adccb69a183123e2750c118cde4c2f
Micxjo Funkcio <micxjo@fastmail.com>
2016-01-03 22:08:41 -0800

    Update lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0"]]}}
+{:user {:mirrors {#"clojars"
+                  {:name "tcrawley"
+                   :url "https://clojars-mirror.tcrawley.org/repo/"}}
+        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [lein-ancient "0.6.8"]
+                  [lein-kibit "0.1.2"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
